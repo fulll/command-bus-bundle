3ed3c77f3b6b86913832181ddf7b0470f3cd6db3
Kevin-koko <kevin.nerino@viacesi.fr>
2024-06-19 12:30:45 +0200

    fix: add typehint

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -21,10 +21,8 @@ class Configuration implements ConfigurationInterface
 
     /**
      * Generates the configuration tree builder.
-     *
-     * @return TreeBuilder The tree builder
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('rezzza_command_bus');
 
